94b7852ca72a0095f224b973154e66342c735924
MohdSaif2884 <166315333+MohdSaif2884@users.noreply.github.com>
2025-04-01 15:26:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName("body")[0]
+// body.style.backgroundColor = "green"
+function setColor(col){
+    body.style.backgroundColor = col;
+
+}
+// setColor("grey")
